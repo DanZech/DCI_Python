7e3e5055d72c0b6dce32d63d873b3c18e678b698
Daniel MZdS <dani.zech@gmail.com>
2023-01-06 10:57:13 +0100

    Just commit

---
+++ January 05/getopt_file.py
@@ -0,0 +1,7 @@
+import sys
+import getopt
+
+file_name = sys.argv[0]
+other_data = sys.argv[1:]
+
+rec_data, not_rec_data = getopt.getopt(other_data, )
---
+++ January_04/main.py
@@ -0,0 +1,11 @@
+import sys
+a = sys.argv
+file_name = a[0]
+#print(a[1])
+print(other_data)
+if '-h' in toher_data or '--help' in other_data:
+    print('usage\n-h or --help for help')
+elif '-a' in other_data or '--activate' in other_data:
+    print('Program activated')
+
+print('end')
