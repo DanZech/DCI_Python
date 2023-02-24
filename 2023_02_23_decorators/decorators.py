692b8ed62a543aa7151f7c8bbd3bfc3fb783aa95
Daniel M.Z.d.S <dani.zech@gmail.com>
2023-02-24 10:03:48 +0100

    asasas

--- 2023_02_23_decorators/decorators.py
+++ 2023_02_23_decorators/decorators.py
@@ -8,4 +8,32 @@ def my_regulr_func():
     print('Hello World')
 
 pretty = make_pretty(my_regulr_func)
-pretty()
+#pretty()
+
+def make_upper(func):
+    def inner():
+        return func().upper()
+    return inner
+
+@make_upper
+def greeting():
+    return 'Hello World'
+
+# print(greeting())
+
+def make_count(func):
+    def inner():
+        return str(len('Hello World'))
+    return inner
+
+def make_split(func):
+    def inner():
+        return func().split()
+    return inner
+
+@make_count
+@make_split
+def ordinary():
+    return 'Hello World'
+
+print(ordinary())
